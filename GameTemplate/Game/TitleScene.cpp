747f363ce306c7d6e2e66367534b6f53f5aa5489
(kato) <(kbc20b13@kawahara.ac.jp)>
2021-06-28 15:30:12 +0900

    hoge

--- GameTemplate/Game/TitleScene.cpp
+++ GameTemplate/Game/TitleScene.cpp
@@ -10,39 +10,31 @@ TitleScene::~TitleScene()
 	DeleteGO(m_exit_Sprite);
 }
 bool TitleScene::Start()
-{	
-m_title_FontRender->SetText(m_title);
-	m_title_FontRender->SetColor(TITLE_COLOR);
-	m_title_FontRender->SetPosition({ SPRITE_TITLE_POSITION });
-	m_title_FontRender->SetScale({ SPRITE_TITLE_SCALE });
+{
+	m_title_Sprite = NewGO<SpriteRender>(0);
+	m_title_Sprite->Init("Assets/Image/TITLE01.dds", 1280, 720);
 
+	m_exit_Sprite = NewGO<SpriteRender>(1);
+	m_exit_Sprite->Init("Assets/Image/EXIT.dds", 252, 420);
+	m_exit_Sprite->SetScale(BALLOON_SIZE);
+	m_exit_Sprite->SetPosition(EXIT_INI_POS);
 
+	m_start_Sprite = NewGO<SpriteRender>(1);
+	m_start_Sprite->Init("Assets/Image/START.dds", 252, 420);
+	m_start_Sprite->SetScale(BALLOON_SIZE);
+	m_start_Sprite->SetPosition(START_INI_POS);
 
-m_start_FontRender->SetText(m_start);
-	m_start_FontRender->SetColor(NOTCHOOSING_COLOR);
-	m_start_FontRender->SetPosition({ SPRITE_START_POSITION });
-	m_start_FontRender->SetScale({ SPRITE_START_SCALE });
+	m_option_Sprite = NewGO<SpriteRender>(1);
+	m_option_Sprite->Init("Assets/Image/OPTION.dds", 252, 420);
+	m_option_Sprite->SetScale(BALLOON_SIZE);
+	m_option_Sprite->SetPosition(OPTION_INI_POS);
 
-m_playernumber_FontRender->SetText(m_playernumber);
-	m_playernumber_FontRender->SetColor(NOTCHOOSING_COLOR);
-	m_playernumber_FontRender->SetPosition({ SPRITE_PLAYERNUM_POSITION });
-	m_playernumber_FontRender->SetScale({ SPRITE_PLAYERNUM_SCALE });
-
-	m_playerCount_FontRender->SetText(std::to_wstring(m_playerCount));
-	m_playerCount_FontRender->SetColor(NOTCHOOSING_COLOR);
-	m_playerCount_FontRender->SetPosition({ SPRITE_PLAYERCOUNT_POSITION });
-	m_playerCount_FontRender->SetScale({ SPRITE_PLAYERCOUNT_SCALE });
-
-	m_exit_FontRender->SetText(m_exit);
-	m_exit_FontRender->SetColor(NOTCHOOSING_COLOR);
-	m_exit_FontRender->SetPosition({ SPRITE_EXIT_POSITION });
-	m_exit_FontRender->SetScale({ SPRITE_EXIT_SCALE });
 	return true;
 }
-	
+
 void TitleScene::Update()
 {
-	if (g_pad[0]->IsTrigger(enButtonRight)){
+	if (g_pad[0]->IsTrigger(enButtonRight)) {
 		m_selectNum += 1;
 		m_selectLoopCount = 0;
 	}
@@ -66,8 +58,8 @@ void TitleScene::Update()
 		break;
 
 	case 1:
-		m_selectLoopCount+=0.05f;
-		m_startPos.y = BALLOON_INI_POSY +(sin(m_selectLoopCount))*40.0f;
+		m_selectLoopCount += 0.05f;
+		m_startPos.y = BALLOON_INI_POSY + (sin(m_selectLoopCount)) * 40.0f;
 		m_exitPos.y = BALLOON_INI_POSY;
 		m_optionPos.y = BALLOON_INI_POSY;
 		break;
@@ -89,19 +81,7 @@ void TitleScene::Update()
 		NewGO<BackGround>(0);
 		DeleteGO(this);
 	}
-if (m_selectNum == 1 && g_pad[0]->IsTrigger(enButtonLeft)) {
-		m_playerCount -= 1;
-		if (m_playerCount < MIN_PLAYERCOUNT) {
-			m_playerCount = MIN_PLAYERCOUNT;
-		}
-		m_playerCount_FontRender->SetText(std::to_wstring(m_playerCount));
-	}
-
-	if (m_selectNum == 1 && g_pad[0]->IsTrigger(enButtonRight)) {
-		m_playerCount += 1;
-		if (m_playerCount > MAX_PLAYERCOUNT) {
-			m_playerCount = MAX_PLAYERCOUNT;
-		}
-		m_playerCount_FontRender->SetText(std::to_wstring(m_playerCount));
-	}
+	m_option_Sprite->SetPosition(m_optionPos);
+	m_start_Sprite->SetPosition(m_startPos);
+	m_exit_Sprite->SetPosition(m_exitPos);
 }
--- GameTemplate/Game/TitleScene.h
+++ GameTemplate/Game/TitleScene.h
@@ -1,26 +1,11 @@
 #pragma once
-namespace 
+namespace
 {
-	Vector4 TITLE_COLOR = { 1,0,0,0 };//タイトルの色
-	Vector4 CHOOSING_COLOR = { 0,1,1,0 };//選択中の色
-	Vector4 NOTCHOOSING_COLOR = { 1,1,1,0 };//選択されてない時の色
-
-
-	const Vector4 TITLE_COLOR = { 1,0,0,0 };//タイトルの色
-	const Vector4 CHOOSING_COLOR = { 0,1,1,0 };//選択中の色
-	const Vector4 NOTCHOOSING_COLOR = { 1,1,1,0 };//選択されてない時の色
-	const Vector2 SPRITE_TITLE_POSITION = { -250,200 };//タイトルの位置
-	const Vector2 SPRITE_START_POSITION = { -85,0 };//スタートボタンの位置
-	const Vector2 SPRITE_PLAYERNUM_POSITION = { -100,-100 };//プレイヤー人数設定ボタンの位置
-	const Vector2 SPRITE_PLAYERCOUNT_POSITION = { 200,-100 };//プレイヤー人数の位置
-	const Vector2 SPRITE_EXIT_POSITION = { -70,-200 };//ゲーム終了ボタンの位置
-	const int SPRITE_TITLE_SCALE = 2;//タイトルの大きさ
-	const int SPRITE_START_SCALE = 1;//スタートボタンの大きさ
-	const int SPRITE_PLAYERNUM_SCALE = 1;//プレイヤー人数設定ボタンの大きさ
-	const int SPRITE_PLAYERCOUNT_SCALE = 1;//プレイヤー人数の大きさ
-	const int SPRITE_EXIT_SCALE = 1;//ゲーム終了ボタンの大きさ
-	const int MIN_PLAYERCOUNT = 1;//最低プレイヤー人数
-	const int MAX_PLAYERCOUNT = 8;//最大プレイヤー人数
+	const Vector3 EXIT_INI_POS = { -300,-200,0 };
+	const Vector3 START_INI_POS = { 0,-200,0 };
+	const Vector3 OPTION_INI_POS = { 300,-200,0 };
+	const Vector3 BALLOON_SIZE = { 0.7f,0.7f,1.0f };
+	const int BALLOON_INI_POSY = -200;
 }
 class TitleScene : public IGameObject
 {
@@ -28,30 +13,19 @@ public:
 	bool Start();
 	void Update();
 	~TitleScene();
-	
-	~TitleScene();
 
 private:
-	FontRender* m_title_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_title = L"TITLE";
-	FontRender* m_playernumber_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_playernumber = L"プレイ人数";
-	FontRender* m_start_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_start = L"はじめる";
-	FontRender* m_exit_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_exit = L"おわる";
-	FontRender* m_title_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_title = L"BALLOON BATTLE";
-	FontRender* m_playernumber_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_playernumber = L"PLAYER";
-	FontRender* m_start_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_start = L"START";
-	FontRender* m_exit_FontRender = NewGO<FontRender>(0);//タイトルの文字
-	std::wstring m_exit = L"EXIT";
+	SpriteRender* m_title_Sprite = NewGO<SpriteRender>(0);
+	SpriteRender* m_start_Sprite = NewGO<SpriteRender>(1);
+	SpriteRender* m_option_Sprite = NewGO<SpriteRender>(1);
+	SpriteRender* m_exit_Sprite = NewGO<SpriteRender>(1);
 
-	FontRender* m_playerCount_FontRender = NewGO<FontRender>(0);//タイトルの文字
+	Vector3 m_exitPos = { -300,-200,0 };
+	Vector3 m_startPos = { 0,-200,0 };
+	Vector3 m_optionPos = { 300,-200,0 };
 
-	int m_playerCount = 4;
-	int m_selectNum = 0;
-
+	int m_playerCount = 1;
+	int m_selectNum = 1;
+	float m_selectLoopCount = 0;
 };
+
